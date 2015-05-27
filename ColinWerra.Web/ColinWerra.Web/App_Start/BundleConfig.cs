80f76d664e7a91b90c149d1c8c6aad84bb2ad9dc
coliwerr <colin.werra@gmail.com>
2015-05-27 11:12:27 -0400

    CSS and JS bundles implementation

---
+++ ColinWerra.Web/ColinWerra.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+
+namespace ColinWerra.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.min.css",
+                "~/Content/styles.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/websitescripts").Include(
+                "~/Scripts/jquery-1.9.1.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/activeTab.js"));
+        }
+    }
+}
